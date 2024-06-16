e5d5acf4db527123ddcdee6dd720accd52f22bbc
qendrimkryeziu <qendrim-kryeziu@outlook.com>
2024-06-16 23:09:56 +0200

    Slideshow for Desktop & Mobile

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,15 +1,18 @@
+import { ProductMobileSlideshow, ProductSlideshow, SizeSelector } from '@/components';
+import QuantitySelector from '@/components/product/quantity-selector/QuantitySelector';
+import { titleFont } from '@/config/fonts';
 import { initialData } from '@/seed/seed';
 import { notFound } from 'next/navigation';
 
 interface Props {
   params: {
     slug: string;
-  }
+  };
 }
 
 
 
-export default function ProductPage({ params }: Props) {
+export default function ProductPage( { params }: Props ) {
 
   const { slug } = params;
   const product = initialData.products.find( product => product.slug === slug );
@@ -20,15 +23,57 @@ export default function ProductPage({ params }: Props) {
 
 
   return (
-    <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
-      
-      {/* Slidershow */}
+    <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3 mb-10">
+
+      {/* Slidershow */ }
       <div className="col-span-1 md:col-span-2">
 
+        {/* Mobile Slideshow */}
+        <ProductMobileSlideshow 
+          title={ product.title }
+          images={ product.images }
+          className="block md:hidden"
+        />
+
+        {/* Desktop Slideshow */}
+        <ProductSlideshow 
+          title={ product.title }
+          images={ product.images }
+          className="hidden md:block"
+        />
       </div>
 
+      {/* Detalles */ }
+      <div className="col-span-1 px-5 ">
+        <h1 className={ ` ${ titleFont.className } antialiased font-bold text-xl` }>
+          { product.title }
+        </h1>
+        <p className="text-lg mb-5">${ product.price }</p>
 
-      {/* Detalles */}
+        {/* Selector de Tallas */ }
+        <SizeSelector
+          selectedSize={ product.sizes[ 1 ] }
+          availableSizes={ product.sizes }
+        />
+
+
+        {/* Selector de Cantidad */ }
+        <QuantitySelector
+          quantity={ 2 }
+        />
+
+
+        {/* Button */ }
+        <button className="btn-primary my-5">
+          Agregar al carrito
+        </button>
+
+        {/* Description */ }
+        <h3 className="font-bold text-sm">Description</h3>
+        <p className="font-light">
+          { product.description }
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,13 @@
 export * from "./products/product-grid/ProductGrid";
 export * from "./products/product-grid/ProductGridItem";
 
+
+export * from "./product/slideshow/ProductSlideshow";
+export * from "./product/slideshow/ProductMobileSlideshow";
+
+export * from "./product/size-selector/SizeSelector";
+export * from "./product/quantity-selector/QuantitySelector";
+
 export * from "./ui/not-found/PageNotFound";
 export * from "./ui/sidebar/Sidebar";
 export * from "./ui/title/Title";
