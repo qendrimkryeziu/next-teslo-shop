0f0c1e77e74c9ac67e70fe07bb95ddcd7fa9fa7b
qendrimkryeziu <qendrim-kryeziu@outlook.com>
2024-06-17 15:13:44 +0200

    Section 15

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,8 +1,26 @@
+import Link from 'next/link';
+import { IoCartOutline } from 'react-icons/io5';
 
 export default function EmptyPage() {
   return (
-    <div>
-      <h1>Empty Page</h1>
+    <div className="flex justify-center items-center h-[800px]">
+      
+      <IoCartOutline size={ 80 } className="mx-6" />
+
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl font-semibold">
+          Tu carrito esta vacio
+        </h1>
+
+        <Link
+          href="/"
+          className="text-blue-500 mt-2 text-4xl"
+          >
+            Regresar
+        </Link>
+      </div>
+
+
     </div>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,8 @@ export * from "./products/product-grid/ProductGrid";
 export * from "./products/product-grid/ProductGridItem";
 
 
+export * from "./ui/footer/Footer";
+
 export * from "./product/slideshow/ProductSlideshow";
 export * from "./product/slideshow/ProductMobileSlideshow";
 
